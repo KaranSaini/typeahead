6242319bdd26fa9f1fe62ec34a6d500a3f820b6d
Karan Saini <kss4382@gmail.com>
2019-10-23 10:37:35 -0400

    The typeahead is now functional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { fromEvent, from, of } from 'rxjs';
+import { map, filter, tap, mergeMap, reduce } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'typeahead';
+  states = ['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware', 'florida', 'georgia', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', 'nebraska', 'nevada', 'new hampshire', 'new jersey', 'new mexico', 'new york', 'north carolina', 'north dakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', 'rhode island', 'south carolina', 'south dakota', 'tennessee', 'texas', 'utah', 'vermont', 'virginia', 'washington', 'west virginia', 'wisconsin', 'wyoming'];
+
+  typeaheadCont;
+
+  onKey(typed) {
+    of(typed)
+      .pipe(
+        map((e): string => e.target.value.toLowerCase()),
+        tap(() => this.typeaheadCont = ''),
+        filter(val => val.length > 2),
+        mergeMap(val =>
+          from(this.states)
+          .pipe(
+            filter(state => state.includes(val)),
+            map(state => state.split(val).join('<b>' + val + "</b>")),
+            reduce((prev: any, state) => prev.concat(state), [])
+          )
+        )
+      )
+      .subscribe(
+        (stateList: string[]) => this.typeaheadCont += '</br>'
+        + stateList.join('</br>')
+      );
+  }
 }
